6c320031e96d622a04b7370d97f99df90f7fe91d
Paul Leslie <66122258+pleslie182@users.noreply.github.com>
2020-11-04 09:52:53 +0000

    Add files via upload

--- angleFinder.js
+++ angleFinder.js
@@ -7,13 +7,5 @@
     const t2 = Math.atan2(rafter, hypot) * 180 / Math.PI;
     const result = Number((t1 - t2)).toFixed(2);
 
-    console.log("height: " + height);
-    console.log("Truss Span: " + trussSpan);
-    console.log("Rafter Depth: " + rafter);
-    console.log("Hyptonuse: " + hypot);
-    console.log("Angle 1: " + t1);
-    console.log("Angle 2: " + t2);
-    console.log("The angle of the truss is: " + result + "°");
-
     document.getElementById("theResult").innerHTML = "This angle is: " + result + "°";
 }
